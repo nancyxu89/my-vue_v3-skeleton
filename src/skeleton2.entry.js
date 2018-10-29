6a01e7f535e753c314331e8a158bcb09661f6a96
徐媛媛 <yuanyuan.xu@ymm56.com>
2018-10-29 17:15:46 +0800

    add skeleton2.vue

--- src/skeleton2.entry.js
+++ src/skeleton2.entry.js
@@ -1,11 +1,11 @@
 /** @format */
 
-import Vue from "vue";
-import Skeleton from "./views/Skeleton2.vue";
+import Vue from 'vue'
+import Skeleton from './views/Skeleton2.vue'
 
 export default new Vue({
-    components: {
-        Skeleton
-    },
-    render: h => h(Skeleton)
-}).$mount("#app")
+	components: {
+		Skeleton
+	},
+	render: h => h(Skeleton)
+}).$mount('#app')
